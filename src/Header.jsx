8088aa2764c08f4d6b385c636d1dd51e9cb43488
zameer sheikh <zameer@skyhive.io>
2023-03-06 10:29:34 +0530

    Header and footer updates

--- src/Header.jsx
+++ src/Header.jsx
@@ -7,7 +7,7 @@ export default function Counter() {
     return (
         <div className="header">
             <div className="header-menu">
-                <div className="title">Fission <span>Devs</span> India</div>
+                <div className="title">Fission <span>Devs</span></div>
                 <ul>
                 <li><a href="#"><i className="fas fa-search"></i></a></li>
                 <li><a href="#"><i className="fas fa-bell"></i></a></li>
